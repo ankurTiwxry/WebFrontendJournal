b54b859f6040858a5b488e1067d35838be7f87e8
pyroNun <33143095+pyroNun@users.noreply.github.com>
2022-07-15 21:23:46 +0200

    Added content to Header.js and removed a few files

--- react_js_tutorial/src/App.js
+++ react_js_tutorial/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      
+      <Header />
     </div>
   );
 }
